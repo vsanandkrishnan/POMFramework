0334e0e3fc73660c2761000919cc6c96d1232420
anand <vs.anandkrishnan@outlook.com>
2020-10-05 06:25:34 -0700

    Enhancement in listener class

--- src/main/java/com/framework/qa/utils/TestAllureListener.java
+++ src/main/java/com/framework/qa/utils/TestAllureListener.java
@@ -1,5 +1,52 @@
 package com.framework.qa.utils;
 
-public class TestAllureListener {
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.internal.annotations.IListeners;
+
+public class TestAllureListener implements ITestListener{
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
